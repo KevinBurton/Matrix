8d2ff65a9cebc144888e8e4daadf8ae62d41cab9
Kevin Burton <rkevinburton@charter.net>
2021-11-14 20:30:27 -0600

    Highlight the result Use CSS to highlight the result

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./styles.css";
 import Matrix from "./Matrix";
+import { useState } from "react";
 
 export default function App() {
+  const columnInput = (e) => {
+    setInputColumn(e.target.value);
+  };
+  const rowInput = (e) => {
+    setInputRow(e.target.value);
+  };
+  const [inputColumn, setInputColumn] = useState(0);
+  const [inputRow, setInputRow] = useState(0);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <Matrix columns={10} rows={10} current={{ column: 4, row: 5 }} />
+      <label htmlFor="inputColumn">Column</label>
+      <input id="inputColumn" type="number" onChange={columnInput} />
+      <label htmlFor="inputRow">Row</label>
+      <input id="inputRow" type="number" onChange={rowInput} />
+      <Matrix
+        columns={10}
+        rows={10}
+        current={{ column: inputColumn, row: inputRow }}
+      />
     </div>
   );
 }
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,20 +1,21 @@
 const Matrix = (props) => {
   const columns = props.columns;
   const rows = props.rows;
-  const currentColumn = props.current.column;
-  const currentRow = props.current.row;
+  const currentColumn = +props.current.column;
+  const currentRow = +props.current.row;
+  console.log(`${currentColumn},${currentRow}`);
   const _array = (n) => {
     return [...Array(10).keys()];
   };
   const tableHeader = _array(columns + 1).map((i) => {
     if (i) {
       return (
-        <td className="header" key={`${i}c`}>
+        <td className="header" key={`${i}ch`}>
           {i.toString().padStart(5, "     ")}
         </td>
       );
     } else {
-      return <td key={"0c"}></td>;
+      return <td key={"0ch"}></td>;
     }
   });
   const tableRows = _array(rows + 1)
@@ -24,12 +25,12 @@ const Matrix = (props) => {
         if (c) {
           if (currentColumn === c && currentRow === r) {
             return (
-              <td className="active" key={`${c}${r}`}>
+              <td className="active" key={`${c}${r}c`}>
                 {(c * r).toString().padStart(5, "     ")}
               </td>
             );
           } else {
-            return <td key={`${c}${r}`}>{c * r}</td>;
+            return <td key={`${c}${r}c`}>{c * r}</td>;
           }
         } else {
           return (
@@ -39,11 +40,13 @@ const Matrix = (props) => {
           );
         }
       });
-      return <tr key={`${r}`}>{tableColumns}</tr>;
+      return <tr key={`${r}r`}>{tableColumns}</tr>;
     });
   return (
     <table>
-      <thead>{tableHeader}</thead>
+      <thead>
+        <tr>{tableHeader}</tr>
+      </thead>
       <tbody>{tableRows}</tbody>
     </table>
   );
